7f762db83918b69477f15eb807715dd8125b1593
Nikhil John <nikhiljohn1010@gmail.com>
2025-11-07 02:01:23 +0530

    fix(models): update functions and plugins logic

--- models/functions.go
+++ models/functions.go
@@ -1,19 +1,11 @@
 package models
 
+type InitialAuthCredentials = map[string]string
 type AuthCredentials = map[string]string
-type MetaData = map[string]any
+type ApiCredentials = []string
 
-type Params struct {
-	SearchText string   `json:"search"`
-	Cursor     string   `json:"cursor"`
-	Sort       bool     `json:"sort,omitempty"`
-	SortOrder  string   `json:"sort_order,omitempty"`
-	SearchIDs  []string `json:"search_ids,omitempty"`
-	// Extra can hold any other parameters that might be needed.
-	Extra map[string]string `json:"extra,omitempty"`
-}
-
-type AuthFunc func(AuthCredentials, Params) error
-type ContactsFunc func(AuthCredentials, Params) (*Contacts, error)
-type LedgerFunc func(AuthCredentials, Params) (*Ledger, error)
+type AuthFunc func(InitialAuthCredentials, AuthParams) (*AuthCredentials, error)
+type ContactsFunc func(AuthCredentials, ContactQueryParams) (*Contacts, error)
+type LedgerFunc func(AuthCredentials, LedgerQueryParams) (*Ledger, error)
+type MetaFunc func() *MetaData
 type HealthFunc func() string
--- models/plugins.go
+++ models/plugins.go
@@ -1,8 +1,11 @@
 package models
 
-import "strings"
+import (
+	"strings"
+)
 
 type DocType string
+type AuthType string
 
 const (
 	DocTypeInvoice      DocType = "invoice"
@@ -12,10 +15,26 @@ const (
 	DocTypeCreditRefund DocType = "credit_refund"
 	DocTypeDebitNote    DocType = "debit_note"
 	DocTypeJournal      DocType = "journal"
+
+	AuthTypeAPIKey AuthType = "api_key"
+	AuthTypeOAuth2 AuthType = "oauth2"
+	AuthTypeNone   AuthType = "none"
 )
 
 func (dt DocType) String() string {
 	return strings.ToTitle(strings.ReplaceAll(string(dt), "_", " "))
+}
+
+type MetaData struct {
+	ID              string           `json:"id"`
+	Name            string           `json:"name"`
+	Version         string           `json:"version"`
+	Description     string           `json:"description,omitempty"`
+	Author          string           `json:"author"`
+	AuthType        AuthType         `json:"auth_type"`
+	ContractVersion string           `json:"contract_version"`
+	AuthCredentials *AuthCredentials `json:"auth_credentials,omitempty"`
+	ApiCredentials  *ApiCredentials  `json:"api_credentials,omitempty"`
 }
 
 type Contact struct {
@@ -25,7 +44,6 @@ type Contact struct {
 }
 
 type Contacts struct {
-	Source     string    `json:"source"`
 	Items      []Contact `json:"contacts"`
 	Count      int       `json:"count"`
 	Total      int       `json:"total"`
